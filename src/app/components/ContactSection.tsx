570de46e05d07fc7e3606e6e87ed536bf62666fa
Aazimiqbal <aazim.iqbal@alphacoders.com>
2024-11-29 17:34:04 +0530

    Footer added

--- src/app/components/ContactSection.tsx
+++ src/app/components/ContactSection.tsx
@@ -1,5 +1,5 @@
 "use client";
-import React, { useState,useEffect} from "react";
+import React, { useState,useEffect } from "react";
 import {
   Phone,
   Mail,
@@ -8,16 +8,24 @@ import {
   Facebook,
   Github,
 } from "lucide-react";
+import { Slider } from "@mui/material";
 
-const ConnectSection: React.FC = (): JSX.Element => {
+const ConnectSection: React.FC = () => {
   const [isPopupOpen, setIsPopupOpen] = useState(false);
-  const [budget, setBudget] = useState(10); // State to track budget range value
+  const [budgetRange, setBudgetRange] = useState<number[]>([10, 100]);
   const [isScrolledToBottom, setIsScrolledToBottom] = useState(false);
+  
+  const handleBudgetRangeChange = (
+    event: Event,
+    newValue: number | number[]
+  ) => {
+    setBudgetRange(newValue as number[]);
+  };
 
   const handlePopupToggle = () => {
     setIsPopupOpen(!isPopupOpen);
   };
-  
+
   // Scroll detection function
   const handleScroll = () => {
     const scrollPosition = window.innerHeight + window.scrollY;
@@ -38,35 +46,6 @@ const ConnectSection: React.FC = (): JSX.Element => {
       window.removeEventListener("scroll", handleScroll);
     };
   }, []);
-  // Function to handle budget input change
-  const handleBudgetChange = (event: React.ChangeEvent<HTMLInputElement>) => {
-    setBudget(Number(event.target.value));
-  };
-
-  // Function to handle min range input change
-  // Define the event type explicitly for 'e'
-  const [minValue, setMinValue] = useState<number>(1000); // Assuming minValue is in thousands
-  const [maxValue, setMaxValue] = useState<number>(50000); // Assuming maxValue is in thousands
-
-  // Function to handle min value change
-  const handleMinChange = (e: React.ChangeEvent<HTMLInputElement>): void => {
-    const newValue = Number(e.target.value); // Ensure the value is a number
-    if (newValue <= maxValue) {
-      setMinValue(newValue); // Update minValue if it's less than or equal to maxValue
-    }
-  };
-
-  // Function to handle max value change
-  const handleMaxChange = (e: React.ChangeEvent<HTMLInputElement>): void => {
-    const newValue = Number(e.target.value); // Ensure the value is a number
-    if (newValue >= minValue) {
-      setMaxValue(newValue); // Update maxValue if it's greater than or equal to minValue
-    }
-  };
-
-  // Function to get the percentage position for the slider
-  const getMinPosition = (value: number): number => (value / 100000) * 100;
-  const getMaxPosition = (value: number): number => (value / 100000) * 100;
 
   return (
     <div className="flex h-full w-full flex-col items-end justify-end bg-white">
@@ -106,67 +85,67 @@ const ConnectSection: React.FC = (): JSX.Element => {
           <button
             aria-labelledby="letsConnectDiv"
             onClick={handlePopupToggle}
-      className="group relative rounded-full bg-[#F2F2F2] overflow-hidden transition-transform p-8 hover:scale-110"
+            className="group relative rounded-full bg-[#F2F2F2] overflow-hidden transition-transform p-8 hover:scale-110"
           >
             {/* First Arrow */}
             <svg
-        xmlns="http://www.w3.org/2000/svg"
-        viewBox="0 0 36 36"
-        fill="none"
-        className="transition-transform duration-300 w-8 h-8 group-hover:translate-x-[250%] group-hover:translate-y-[-250%]"
-      >
-        <path
-          fill="url(#service_arrow_color)"
-          fillRule="evenodd"
-          d="M35.892.81H2.108v5.631h24.171L.941 31.78l3.982 3.982L30.26 10.423v24.172h5.63V.81Z"
-          clipRule="evenodd"
-        />
-        <defs>
-          <linearGradient
-            id="service_arrow_color"
-            x1="0.086"
-            x2="35.104"
-            y1="25.3"
-            y2="25.3"
-            gradientUnits="userSpaceOnUse"
-          >
-            <stop stopColor="#BF00FF" />
-            <stop offset="0.06" stopColor="#C306F1" />
-            <stop offset="0.53" stopColor="#E3318C" />
-            <stop offset="0.85" stopColor="#F74C4C" />
-            <stop offset="1" stopColor="#FF5633" />
-          </linearGradient>
-        </defs>
-      </svg>
-      <svg
-        xmlns="http://www.w3.org/2000/svg"
-        viewBox="0 0 36 36"
-        fill="none"
-        className="absolute translate-x-[-250%] translate-y-[250%] transition-transform duration-300 inset-8 w-8 h-8 group-hover:translate-x-0 group-hover:translate-y-0"
-      >
-        <path
-          fill="url(#service_arrow_color)"
-          fillRule="evenodd"
-          d="M35.892.81H2.108v5.631h24.171L.941 31.78l3.982 3.982L30.26 10.423v24.172h5.63V.81Z"
-          clipRule="evenodd"
-        />
-        <defs>
-          <linearGradient
-            id="service_arrow_color"
-            x1="0.086"
-            x2="35.104"
-            y1="25.3"
-            y2="25.3"
-            gradientUnits="userSpaceOnUse"
-          >
-            <stop stopColor="#BF00FF" />
-            <stop offset="0.06" stopColor="#C306F1" />
-            <stop offset="0.53" stopColor="#E3318C" />
-            <stop offset="0.85" stopColor="#F74C4C" />
-            <stop offset="1" stopColor="#FF5633" />
-          </linearGradient>
-        </defs>
-      </svg>
+              xmlns="http://www.w3.org/2000/svg"
+              viewBox="0 0 36 36"
+              fill="none"
+              className="transition-transform duration-300 w-8 h-8 group-hover:translate-x-[250%] group-hover:translate-y-[-250%]"
+            >
+              <path
+                fill="url(#service_arrow_color)"
+                fillRule="evenodd"
+                d="M35.892.81H2.108v5.631h24.171L.941 31.78l3.982 3.982L30.26 10.423v24.172h5.63V.81Z"
+                clipRule="evenodd"
+              />
+              <defs>
+                <linearGradient
+                  id="service_arrow_color"
+                  x1="0.086"
+                  x2="35.104"
+                  y1="25.3"
+                  y2="25.3"
+                  gradientUnits="userSpaceOnUse"
+                >
+                  <stop stopColor="#BF00FF" />
+                  <stop offset="0.06" stopColor="#C306F1" />
+                  <stop offset="0.53" stopColor="#E3318C" />
+                  <stop offset="0.85" stopColor="#F74C4C" />
+                  <stop offset="1" stopColor="#FF5633" />
+                </linearGradient>
+              </defs>
+            </svg>
+            <svg
+              xmlns="http://www.w3.org/2000/svg"
+              viewBox="0 0 36 36"
+              fill="none"
+              className="absolute translate-x-[-250%] translate-y-[250%] transition-transform duration-300 inset-8 w-8 h-8 group-hover:translate-x-0 group-hover:translate-y-0"
+            >
+              <path
+                fill="url(#service_arrow_color)"
+                fillRule="evenodd"
+                d="M35.892.81H2.108v5.631h24.171L.941 31.78l3.982 3.982L30.26 10.423v24.172h5.63V.81Z"
+                clipRule="evenodd"
+              />
+              <defs>
+                <linearGradient
+                  id="service_arrow_color"
+                  x1="0.086"
+                  x2="35.104"
+                  y1="25.3"
+                  y2="25.3"
+                  gradientUnits="userSpaceOnUse"
+                >
+                  <stop stopColor="#BF00FF" />
+                  <stop offset="0.06" stopColor="#C306F1" />
+                  <stop offset="0.53" stopColor="#E3318C" />
+                  <stop offset="0.85" stopColor="#F74C4C" />
+                  <stop offset="1" stopColor="#FF5633" />
+                </linearGradient>
+              </defs>
+            </svg>
           </button>
         </div>
         {/* Contact Section */}
@@ -338,102 +317,19 @@ const ConnectSection: React.FC = (): JSX.Element => {
               </div>
             </div>
 
-            <div className="mt-[1.5rem]">
-      <label htmlFor="budgetRange" className="text-black text-lg">
-        Budget (₹ in thousands)
-      </label>
-      <div className="price-range-slider relative pt-[1rem]">
-        <div className="slider relative">
-          <div
-            className="example-track example-track-0"
-            style={{
-              position: 'absolute',
-              left: '0px',
-              right: `${1000 - getMaxPosition(maxValue)}%`,
-            }}
-          ></div>
-          <div
-            className="example-track example-track-1"
-            style={{
-              position: 'absolute',
-              left: `${getMinPosition(minValue)}%`,
-              right: `${1000 - getMaxPosition(maxValue)}%`,
-            }}
-          ></div>
-          <div
-            className="example-track example-track-2"
-            style={{
-              position: 'absolute',
-              left: `${getMinPosition(minValue)}%`,
-              right: '0px',
-            }}
-          ></div>
-
-          {/* Min Value Slider Handle */}
-          <div
-            className="group absolute -top-2 cursor-pointer rounded-full bg-blue-500"
-            style={{
-              left: `${getMinPosition(minValue)}%`,
-              zIndex: 1, // Make sure minValue handle has a lower z-index than maxValue handle
-            }}
-            role="slider"
-            aria-valuenow={minValue}
-            aria-valuemin={1}
-            aria-valuemax={maxValue}
-            aria-labelledby="minValueLabel"
-          >
-            <div className="absolute top-8 flex -translate-x-1/2 items-center justify-center rounded-b-3xl border border-[#E6E6E6] bg-white p-[0.5rem] font-Montserrat text-sm">
-              ₹{minValue / 1000}k
+            <div className="mt-4">
+              <label className="text-black text-lg">Budget</label>
+              <div className="flex items-center justify-between mt-3">
+                <Slider
+                  getAriaLabel={() => "Budget range"}
+                  value={budgetRange}
+                  onChange={handleBudgetRangeChange}
+                  valueLabelDisplay="auto"
+                  min={0}
+                  max={1000}
+                />
+              </div>
             </div>
-          </div>
-
-          {/* Max Value Slider Handle */}
-          <div
-            className="group absolute -top-2 cursor-pointer rounded-full bg-blue-500"
-            style={{
-              left: `${getMaxPosition(maxValue)}%`,
-              zIndex: 2, // Ensure maxValue handle is on top
-            }}
-            role="slider"
-            aria-valuenow={maxValue}
-            aria-valuemin={minValue}
-            aria-valuemax={100000}
-            aria-labelledby="maxValueLabel"
-          >
-            <div className="absolute top-8 flex -translate-x-1/2 items-center justify-center rounded-b-3xl border border-[#E6E6E6] bg-white p-[0.5rem] font-Montserrat text-sm">
-              ₹{maxValue / 1000}k+
-            </div>
-          </div>
-        </div>
-
-        {/* Range Inputs for Min and Max */}
-        <div className="absolute inset-x-0 top-[1rem]">
-          <input
-            type="range"
-            min="1"
-            max="100000"
-            value={minValue}
-            onChange={handleMinChange}
-            className="absolute w-full h-2 bg-gray-300 rounded-full focus:outline-none z-10" // z-index added to avoid overlap
-          />
-          <input
-            type="range"
-            min={minValue + 1}
-            max="100000"
-            value={maxValue}
-            onChange={handleMaxChange}
-            className="absolute w-full h-2 bg-gray-300 rounded-full focus:outline-none z-20" // Higher z-index for max value
-          />
-        </div>
-      </div>
-
-      <p className="text-sm text-gray-500 mt-2">
-        Selected Budget: ₹{minValue / 1000}k to ₹{maxValue / 1000}k
-      </p>
-    </div>
-
-
-
 
             <div className="mt-4">
               <label className="text-black text-lg">Enquiry</label>
@@ -453,8 +349,6 @@ const ConnectSection: React.FC = (): JSX.Element => {
           </form>
         </div>
       </div>
-      
-
       <div
         className={`fixed inset-x-0 bottom-0 z-40 transition-all duration-500 ease-in-out ${
           isScrolledToBottom ? "transform translate-y-0" : "transform translate-y-full"
@@ -473,7 +367,6 @@ const ConnectSection: React.FC = (): JSX.Element => {
     
     </div>
     </div>
-    
   );
 };
 
