bcc5c8b1a493723e2013248789b36a2d17b7b572
Rahat Nadeem <rahat.nadeem@alphacoders.co.in>
2024-12-26 16:58:54 +0530

    added new images

--- src/app/data/StackCardsData.ts
+++ src/app/data/StackCardsData.ts
@@ -25,7 +25,7 @@ export const cards: CardStatsProps[] = [
       "Every groundbreaking solution begins with a spark of imagination. At AlphaCoders we cultivate your ideas, explore creative possibilities, and turn aspirations into actionable",
     isEven: true,
     path:"/weimagine",
-    color: "bg-[#4C585B]",
+    color: "bg-[#C63C51]",
   },
   {
     imageSrc: "wedesign.jpg",
@@ -39,7 +39,7 @@ export const cards: CardStatsProps[] = [
     subtitle: "Designing for Impact and Experience ", // Added subtitle here
     isEven: false,
     path:"/wedesign",
-    color: "bg-[#4C585B]",
+    color: "bg-[#003161]",
     description:
       "Design is where imagination takes form. Our team crafts thoughtful, user-centric designs that are as functional as they are beautiful. Each design we create solves problems, enhances experiences, and stands the test of time.",
   },
@@ -50,7 +50,7 @@ export const cards: CardStatsProps[] = [
     subtitle: "Creation Brings Your Vision to Life", // Added subtitle here
     isEven: true,
     path:"/wecreate",
-    color: "bg-[#4C585B]",
+    color: "bg-[#7A1CAC]",
     description:
       "Our expertise lies in building what you dream. With cutting-edge technologies, robust engineering, and meticulous execution, we bring your ideas into reality. From software solutions to digital platforms, we deliver with precision and excellence",
   },
