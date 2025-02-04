926b0427be5db9fce62e7f4732a1d1e7fd43b096
Aazimiqbal <aazim.iqbal@alphacoders.com>
2025-02-04 11:33:30 +0530

    updated seo

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -8,7 +8,7 @@ import { Metadata } from "next";
 
 export const metadata: Metadata = {
   title: {
-    default: "AlphaCoders - Innovative Digital Solutions",
+    default: "AlphaCoders",
     template: "%s | AlphaCoders",
     absolute: "",
   },
