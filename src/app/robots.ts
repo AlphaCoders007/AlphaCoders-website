0c06fa7f0659e1813e297b92d3278a716a6341b9
Aazimiqbal <aazim.iqbal@alphacoders.com>
2025-02-09 21:33:46 +0530

    updated seo

--- src/app/robots.ts
+++ src/app/robots.ts
@@ -7,6 +7,6 @@ export default function robots(): MetadataRoute.Robots {
       allow: '/',
       disallow: '/private/ ',
     },
-    sitemap: 'https://alphacodersinc.com/sitemap.xml',
+    sitemap: 'https://www.alphacoders.co.in/sitemap.xml',
   }
 }
--- src/app/sitemap.ts
+++ src/app/sitemap.ts
@@ -3,25 +3,25 @@ import type { MetadataRoute } from 'next'
 export default function sitemap(): MetadataRoute.Sitemap {
   return [
     {
-      url: 'https://alphacodersinc.com/',
+      url: 'https://www.alphacoders.co.in/',
       lastModified: new Date(),
       changeFrequency: 'yearly',
       priority:0.9,
     },
     {
-      url: 'https://alphacodersinc.com/weimagine',
+      url: 'https://www.alphacoders.co.in/weimagine',
       lastModified: new Date(),
       changeFrequency: 'monthly',
       priority: 0.8,
     },
     {
-      url: 'https://alphacodersinc.com/wedesign',
+      url: 'https://www.alphacoders.co.in/wedesign',
       lastModified: new Date(),
       changeFrequency: 'monthly',
       priority: 0.8,
     },
     {
-      url: 'https://alphacodersinc.com/wecreate',
+      url: 'https://www.alphacoders.co.in/wecreate',
       lastModified: new Date(),
       changeFrequency: 'monthly',
       priority: 0.8,
